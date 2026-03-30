273854fc5eed27bf47e53ee4ec68594ac8f83dda
Alexus Loera <alexusloera1171382@gmail.com>
2026-03-30 00:07:56 -0700

    Initial commit

---
+++ src/Components/SideNav.jsx
@@ -0,0 +1,12 @@
+import CryptoNews from "./CryptoNews"
+
+function SideNav() {
+  return (
+    <div className="sidenav">
+      <h1>here is my side nav bar</h1>
+      <CryptoNews />
+    </div>
+  )
+}
+
+export default SideNav
